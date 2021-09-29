7c274baedb7898865c9ec2c5bbd07903ee26004e
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-29 15:16:55 +0700

    Create tebakbendera_ID.js

---
+++ Game/tebakbendera_ID.js
@@ -0,0 +1,22 @@
+[
+  {
+    "index": 0,
+    "soal": " Pertanyaan",
+    "jawaban": " Jawaban"
+  },
+  {
+   "index": 1,
+   "soal": "🇨🇳", 
+   "jawaban": "china"
+  }, 
+  {
+    "index": 2,
+    "soal": "🇨🇷", 
+    "jawaban": "thailand"
+   }, 
+   {
+    "index": 3,
+    "soal", "🇲🇨", 
+    "jawaban": "Polandia"
+   }
+]
