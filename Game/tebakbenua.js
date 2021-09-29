8c9ce024c5dd6027bf7642100c9169446e16761a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-09-29 15:36:28 +0700

    Update tebakbenua.js

--- Game/tebakbenua.js
+++ Game/tebakbenua.js
@@ -8,5 +8,10 @@
    "index": 1,
    "soal": "benua pertama yg Ada didunia adalah... ", 
    "jawaban": "eurasia"
+  }, 
+  {
+   "index": 2,
+   "soal": "benua Asia merupakan benua.... ", 
+   "jawaban": " Terbesar",
   }
 ]
