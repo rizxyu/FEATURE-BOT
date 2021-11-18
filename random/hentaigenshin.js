22e3655585c8b5f36f4560921e66b24e5b47e1c5
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-18 10:35:06 +0700

    Create hentaigenshin.js

---
+++ random/hentaigenshin.js
@@ -0,0 +1,13 @@
+[
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8XvGO_ykDexjxVLu6UyCPt1zGvOCsIv2Y7A&usqp=CAU",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiewOcsO26-2x5GnlkMG2etltTBsEK-uuHFw&usqp=CAU"0,
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+]
