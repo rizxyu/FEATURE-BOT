dc973a8b3602304ede8165a923838541f0f08516
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-18 10:44:27 +0700

    Update hentaigenshin.js

--- random/hentaigenshin.js
+++ random/hentaigenshin.js
@@ -1,13 +1,6 @@
 [
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8XvGO_ykDexjxVLu6UyCPt1zGvOCsIv2Y7A&usqp=CAU",
 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiewOcsO26-2x5GnlkMG2etltTBsEK-uuHFw&usqp=CAU"0,
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-"",
-""
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgRPlvA4iCzshokZzai_gkeg2XMhqn8Pxghw&usqp=CAU",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd6J_IpSd1qF-lCazY8-eMsEE_7VsYlLP6Uw&usqp=CAU"
 ]
